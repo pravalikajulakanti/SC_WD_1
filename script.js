cd1d5ea7305eef42678bc5299db8046c79cc589c
pravalika julakanti <108797363+pravalikajulakanti@users.noreply.github.com>
2024-11-02 20:37:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    var navbar = document.getElementById("navbar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+};
